ef124817acb5d29d888c743c009b7f5c2028db10
Awmfara <69292567+Awmfara@users.noreply.github.com>
2020-12-09 11:05:29 +1100

    FinalProject

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private int money;
+    public int Money
+    {
+        get
+        {
+            return money;
+        }
+        set
+        {
+            money = value;
+        }
+    }
+
+
+    private int typeOfTower;
+    public int TypeOfTower { get { return typeOfTower; } set { typeOfTower = value; } }
+    
+    public List<Enemy> enemies=new List<Enemy>();
+    public List<Tower> towers=new List<Tower>();
+
+    [SerializeField]
+    private int towerCostMilk = default;
+    public int TowerCostMilk => towerCostMilk;
+    
+    [SerializeField]
+
+    private int towerCostEgg = default;
+    public int TowerCostEgg => towerCostEgg;
+
+    [SerializeField]
+    GameObject killScreen;
+
+    private void Start()
+    {
+        typeOfTower = 1;
+    }
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        killScreen.SetActive(true);
+    }
+    public void ResetStat()
+    {
+        money = 100;
+
+    }
+
+}
+
+
+
